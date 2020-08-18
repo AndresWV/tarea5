8720b02c5ca5a7abf261b4a6c1a541e175dd5b16
AndresWV <52219370+AndresWV@users.noreply.github.com>
2020-08-17 22:03:31 -0400

    Lectura de los datos

--- Grafos/src/grafos/Grafos.java
+++ Grafos/src/grafos/Grafos.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package grafos;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +17,45 @@ public class Grafos {
     /**
      * @param args the command line arguments
      */
+     
+    static File f = new File("grafos.txt");
+    static ArrayList<int[]> posicionesVertices = new ArrayList<int[]>();
+    static ArrayList<int[]> aristasVertices = new ArrayList<int[]>();
+    static int n;
+    static int m;
+    
+    
+    public static void extraerDatos(){
+        
+        try(Scanner entrada = new Scanner(f)){
+            
+            n = Integer.parseInt(entrada.next());
+            
+           
+            for (int i = 0; i < 2*n; i++) {   
+                String numero = entrada.next();
+                int [] pos = new int [2]; 
+                pos[0] = Character.getNumericValue(numero.charAt(0));
+                pos[1] = Character.getNumericValue(numero.charAt(2));
+                posicionesVertices.add(pos);
+            }
+            m = Integer.parseInt(entrada.next());
+            for (int i = 0; i < m; i++) {
+                int [] pos = new int [2]; 
+                pos[0] = Integer.parseInt(entrada.next());
+                pos[1] = Integer.parseInt(entrada.next());
+                aristasVertices.add(pos);
+            }
+            
+        }
+        catch(Exception e){
+            System.out.println(e.getCause());
+        }
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        extraerDatos();
+        
     }
     
 }
